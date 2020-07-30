d6d7716468bd2a49d473ea9c4364820dd00ecb3d
Ranjithprabhu <rkumar53@slb.com>
2020-07-30 18:39:54 +0530

    bootstrap added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,12 @@
 import * as React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 export interface IButtonProps {
     children?: React.ReactNode,
     onClick?: (e: any) => void
 }
-const styles = {
-    border: '1px solid #eee',
-    borderRadius: 3,
-    backgroundColor: '#FFFFFF',
-    cursor: 'pointer',
-    fontSize: 15,
-    padding: '3px 10px',
-    margin: 10,
-};
+
 const Button: React.SFC<IButtonProps> = (props) => (
-    <button onClick={props.onClick} style={styles} type="button">
+    <button onClick={props.onClick} className={'btn btn-primary'} type="button">
         {props.children}
     </button>
 );
